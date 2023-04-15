92ec7ef909b1b04b663b8cbba4beceadc0ad5331
teamgram.io <74940245+teamgramio@users.noreply.github.com>
2023-04-15 16:06:46 +0800

    feat(mtproto): Update API scheme on layer 157.

--- mtproto/chat_banned_rights.go
+++ mtproto/chat_banned_rights.go
@@ -22,53 +22,56 @@ import (
 	"math"
 )
 
-// chatBannedRights#9f120418 flags:#
-//	view_messages:flags.0?true
-//	send_messages:flags.1?true
-//	send_media:flags.2?true
-//	send_stickers:flags.3?true
-//	send_gifs:flags.4?true
-//	send_games:flags.5?true
-//	send_inline:flags.6?true
-//	embed_links:flags.7?true
-//	send_polls:flags.8?true
-//	change_info:flags.10?true
-//	invite_users:flags.15?true
-//	pin_messages:flags.17?true
-//	until_date:int = ChatBannedRights;
-
 const (
-	BANNED_VIEW_MESSAGES = 1 << 0
-	BANNED_SEND_MESSAGES = 1 << 1
-	BANNED_SEND_MEDIA    = 1 << 2
-	BANNED_SEND_STICKERS = 1 << 3
-	BANNED_SEND_GIFS     = 1 << 4
-	BANNED_SEND_GAMES    = 1 << 5
-	BANNED_SEND_INLINE   = 1 << 6
-	BANNED_EMBED_LINKS   = 1 << 7
-	BANNED_SEND_POLLS    = 1 << 8
-	BANNED_CHANGE_INFO   = 1 << 10
-	BANNED_INVITE_USERS  = 1 << 15
-	BANNED_PIN_MESSAGES  = 1 << 17
+	BANNED_VIEW_MESSAGES    = 1 << 0
+	BANNED_SEND_MESSAGES    = 1 << 1
+	BANNED_SEND_MEDIA       = 1 << 2
+	BANNED_SEND_STICKERS    = 1 << 3
+	BANNED_SEND_GIFS        = 1 << 4
+	BANNED_SEND_GAMES       = 1 << 5
+	BANNED_SEND_INLINE      = 1 << 6
+	BANNED_EMBED_LINKS      = 1 << 7
+	BANNED_SEND_POLLS       = 1 << 8
+	BANNED_CHANGE_INFO      = 1 << 10
+	BANNED_INVITE_USERS     = 1 << 15
+	BANNED_PIN_MESSAGES     = 1 << 17
+	BANNED_MANAGE_TOPICS    = 1 << 18
+	BANNED_SEND_PHOTOS      = 1 << 19
+	BANNED_SEND_VIDEOS      = 1 << 20
+	BANNED_SEND_ROUNDVIDEOS = 1 << 21
+	BANNED_SEND_AUDIOS      = 1 << 22
+	BANNED_SEND_VOICES      = 1 << 23
+	BANNED_SEND_DOCS        = 1 << 24
+	BANNED_SEND_PLAIN       = 1 << 25
 )
 
-type BannedRights int64
+type (
+	BannedRights int64
+)
 
 func MakeDefaultBannedRights() *ChatBannedRights {
 	return MakeTLChatBannedRights(&ChatBannedRights{
-		ViewMessages: false,
-		SendMessages: false,
-		SendMedia:    false,
-		SendStickers: false,
-		SendGifs:     false,
-		SendGames:    false,
-		SendInline:   false,
-		EmbedLinks:   false,
-		SendPolls:    false,
-		ChangeInfo:   false,
-		InviteUsers:  false,
-		PinMessages:  false,
-		UntilDate:    math.MaxInt32,
+		ViewMessages:    false,
+		SendMessages:    false,
+		SendMedia:       false,
+		SendStickers:    false,
+		SendGifs:        false,
+		SendGames:       false,
+		SendInline:      false,
+		EmbedLinks:      false,
+		SendPolls:       false,
+		ChangeInfo:      false,
+		InviteUsers:     false,
+		PinMessages:     false,
+		ManageTopics:    false,
+		SendPhotos:      false,
+		SendVideos:      false,
+		SendRoundvideos: false,
+		SendAudios:      false,
+		SendVoices:      false,
+		SendDocs:        false,
+		SendPlain:       false,
+		UntilDate:       math.MaxInt32,
 	}).To_ChatBannedRights()
 }
 
@@ -78,33 +81,51 @@ func (m BannedRights) ToChatBannedRights() *ChatBannedRights {
 	}
 
 	return MakeTLChatBannedRights(&ChatBannedRights{
-		ViewMessages: (m & BANNED_VIEW_MESSAGES) != 0,
-		SendMessages: (m & BANNED_SEND_MESSAGES) != 0,
-		SendMedia:    (m & BANNED_SEND_MEDIA) != 0,
-		SendStickers: (m & BANNED_SEND_STICKERS) != 0,
-		SendGifs:     (m & BANNED_SEND_GIFS) != 0,
-		SendGames:    (m & BANNED_SEND_GAMES) != 0,
-		SendInline:   (m & BANNED_SEND_INLINE) != 0,
-		EmbedLinks:   (m & BANNED_EMBED_LINKS) != 0,
-		SendPolls:    (m & BANNED_SEND_POLLS) != 0,
-		ChangeInfo:   (m & BANNED_CHANGE_INFO) != 0,
-		InviteUsers:  (m & BANNED_INVITE_USERS) != 0,
-		PinMessages:  (m & BANNED_PIN_MESSAGES) != 0,
-		UntilDate:    int32(m >> 32),
+		ViewMessages:    (m & BANNED_VIEW_MESSAGES) != 0,
+		SendMessages:    (m & BANNED_SEND_MESSAGES) != 0,
+		SendMedia:       (m & BANNED_SEND_MEDIA) != 0,
+		SendStickers:    (m & BANNED_SEND_STICKERS) != 0,
+		SendGifs:        (m & BANNED_SEND_GIFS) != 0,
+		SendGames:       (m & BANNED_SEND_GAMES) != 0,
+		SendInline:      (m & BANNED_SEND_INLINE) != 0,
+		EmbedLinks:      (m & BANNED_EMBED_LINKS) != 0,
+		SendPolls:       (m & BANNED_SEND_POLLS) != 0,
+		ChangeInfo:      (m & BANNED_CHANGE_INFO) != 0,
+		InviteUsers:     (m & BANNED_INVITE_USERS) != 0,
+		PinMessages:     (m & BANNED_PIN_MESSAGES) != 0,
+		ManageTopics:    (m & BANNED_MANAGE_TOPICS) != 0,
+		SendPhotos:      (m & BANNED_SEND_PHOTOS) != 0,
+		SendVideos:      (m & BANNED_SEND_VIDEOS) != 0,
+		SendRoundvideos: (m & BANNED_SEND_ROUNDVIDEOS) != 0,
+		SendAudios:      (m & BANNED_SEND_AUDIOS) != 0,
+		SendVoices:      (m & BANNED_SEND_VOICES) != 0,
+		SendDocs:        (m & BANNED_SEND_DOCS) != 0,
+		SendPlain:       (m & BANNED_SEND_PLAIN) != 0,
+		UntilDate:       int32(m >> 32),
 	}).To_ChatBannedRights()
 }
 
 func (m *ChatBannedRights) hasRights() bool {
 	return m.ViewMessages ||
 		m.SendMessages ||
+		m.SendMedia ||
 		m.SendStickers ||
 		m.SendGifs ||
 		m.SendGames ||
+		m.SendInline ||
 		m.EmbedLinks ||
 		m.SendPolls ||
 		m.ChangeInfo ||
 		m.InviteUsers ||
-		m.PinMessages
+		m.PinMessages ||
+		m.ManageTopics ||
+		m.SendPhotos ||
+		m.SendVideos ||
+		m.SendRoundvideos ||
+		m.SendAudios ||
+		m.SendVoices ||
+		m.SendDocs ||
+		m.SendPlain
 }
 
 func (m *ChatBannedRights) NoBanRights() bool {
@@ -171,6 +192,38 @@ func (m *ChatBannedRights) CanPinMessages(date int32) bool {
 	return !m.GetPinMessages() || date >= m.UntilDate
 }
 
+func (m *ChatBannedRights) CanManageTopics(date int32) bool {
+	return !m.GetManageTopics() || date >= m.UntilDate
+}
+
+func (m *ChatBannedRights) CanSendPhotos(date int32) bool {
+	return !m.GetSendPhotos() || date >= m.UntilDate
+}
+
+func (m *ChatBannedRights) CanSendVideos(date int32) bool {
+	return !m.GetSendVideos() || date >= m.UntilDate
+}
+
+func (m *ChatBannedRights) CanSendRoundvideos(date int32) bool {
+	return !m.GetSendRoundvideos() || date >= m.UntilDate
+}
+
+func (m *ChatBannedRights) CanSendAudios(date int32) bool {
+	return !m.GetSendAudios() || date >= m.UntilDate
+}
+
+func (m *ChatBannedRights) CanSendVoices(date int32) bool {
+	return !m.GetSendVoices() || date >= m.UntilDate
+}
+
+func (m *ChatBannedRights) CanSendDocs(date int32) bool {
+	return !m.GetSendDocs() || date >= m.UntilDate
+}
+
+func (m *ChatBannedRights) CanSendPlain(date int32) bool {
+	return !m.GetSendPlain() || date >= m.UntilDate
+}
+
 func (m *ChatAdminRights) DisallowChannel() bool {
 	// return m.CanPostMessages() || m.CanEditMessages()
 	return false
@@ -216,6 +269,30 @@ func (m *ChatBannedRights) ToBannedRights() BannedRights {
 	}
 	if m.GetPinMessages() {
 		rights |= BANNED_PIN_MESSAGES
+	}
+	if m.GetManageTopics() {
+		rights |= BANNED_MANAGE_TOPICS
+	}
+	if m.GetSendPhotos() {
+		rights |= BANNED_SEND_PHOTOS
+	}
+	if m.GetSendVideos() {
+		rights |= BANNED_SEND_VIDEOS
+	}
+	if m.GetSendRoundvideos() {
+		rights |= BANNED_SEND_ROUNDVIDEOS
+	}
+	if m.GetSendAudios() {
+		rights |= BANNED_SEND_AUDIOS
+	}
+	if m.GetSendVoices() {
+		rights |= BANNED_SEND_VOICES
+	}
+	if m.GetSendDocs() {
+		rights |= BANNED_SEND_DOCS
+	}
+	if m.GetSendPlain() {
+		rights |= BANNED_SEND_PLAIN
 	}
 
 	untilDate := m.GetUntilDate()
